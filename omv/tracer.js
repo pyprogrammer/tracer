fb236f74e6cff0f4351960b4829e73ff3333a349
Nathan Zhang <nzhang32@berkeley.edu>
2016-10-05 19:43:03 -0700

    added trusted detection

--- omv/tracer.js
+++ omv/tracer.js
@@ -1,8 +1,29 @@
 (function () {
    // TODO: Hide vars
+
+   var trusted = [
+      "localhost"
+   ];
+
+   var untrusted = [
+      "localhost:8000/adblock-breaker.js"
+   ];
+
    traces = {
       "createElement": []
    };
+
+   function isTrusted(trace) {
+      for (var i = 0; i < trace.length; i++) {
+         for (var j = 0; j < untrusted.length; j++) {
+            if ((trace[i].fileName.indexOf(untrusted[j])) != -1) {
+               return false;
+            }
+         }
+      }
+      return true;
+   }
+
    dataTags = {};
    tags = 0;
    var old_createElement = document.createElement;
@@ -12,7 +33,7 @@
       traces["createElement"].push(trace);
       var el = old_createElement.call(document, tagName);
       el.attributes["metadata-tag"] = tags;
-      dataTags[tags] = trace;
+      dataTags[tags] = isTrusted(trace);
       tags += 1;
       return el;
    };
