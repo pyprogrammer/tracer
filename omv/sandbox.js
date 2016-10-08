74347cc2da41e2cc1c32c2275f2452e449083992
Michael Anthony Knyszek <mknyszek@gmail.com>
2016-10-07 20:40:12 -0700

    Create new script

--- omv/sandbox.js
+++ omv/sandbox.js
@@ -7,16 +7,19 @@ function generateWrapper(codeString) {
 }
 
 function sandbox(scriptTag) {
+   var newScript = document.createElement('script');
    if (scriptTag.hasAttribute('src')) {
       var scriptURL = scriptTag.getAttribute('src');
       var request = new XMLHttpRequest();
       request.open('GET', scriptURL, false);
       request.send();
       scriptTag.removeAttribute('src');
-      scriptTag.innerHTML = generateWrapper(request.responseText);
+      newScript.innerHTML = generateWrapper(request.responseText);
    } else {
-      scriptTag.innerHTML = generateWrapper(scriptTag.innerHTML);
+      newScript.innerHTML = generateWrapper(scriptTag.innerHTML);
    }
+   scriptTag.parentNode.insertBefore(newScript, scriptTag);
+   scriptTag.parentNode.removeChild(scriptTag);
 }
 
 function getEval(context) {
